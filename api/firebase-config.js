163ec7abf8228384dc31ad9378b2c3912d7bae2a
zicceasd <136277491+zicce@users.noreply.github.com>
2026-01-02 01:29:09 +0200

    555
    555

---
+++ api/firebase-config.js
@@ -0,0 +1,12 @@
+
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDolzQZPSdzWQiYbqjhlyNkjE9LrYxkb7E",
+  authDomain: "solaris-database-b5384.firebaseapp.com",
+  projectId: "solaris-database-b5384",
+  storageBucket: "solaris-database-b5384.firebasestorage.app",
+  messagingSenderId: "229880902004",
+  appId: "1:229880902004:web:34be022f36ac4157433c21",
+  measurementId: "G-27L86KFYG6"
+};
+
